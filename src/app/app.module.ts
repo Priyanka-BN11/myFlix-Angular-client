9c8f4f1c87e49f3eb654e75721ab6274df2be828
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2022-11-27 11:56:25 +0100

    Added angular material (6.4 task)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MatIconModule } from '@angular/material/icon';
 
 import { RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
@@ -48,6 +49,7 @@ const appRoutes: Routes = [
     MatDialogModule,
     MatSnackBarModule,
     FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/super-div.component.ts
+++ src/app/super-div.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-super-div',
-  templateUrl: './super-div.component.html',
-  styleUrls: ['./super-div.component.scss']
-})
-export class SuperDivComponent {
-  constructor() {
-  }
+// @Component({
+//   selector: 'app-super-div',
+//   templateUrl: './super-div.component.html',
+//   styleUrls: ['./super-div.component.scss']
+// })
+// export class SuperDivComponent {
+//   constructor() {
+//   }
 
-}
+// }
